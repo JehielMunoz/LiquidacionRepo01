30b80233e2239a041a54e8542619a4e70826b91b
Jehiel Muñoz <jehiel_926@hotmail.com>
2016-09-15 22:32:38 -0400

    VistaPrevia

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-function cboton(){
-	document.getElementsByClassName("bAgregar").backgroundColor="#1b95e0";
+var ctrl = true;
+
+function cBoton(Id){
+	if(ctrl==true){
+		ctrl=false;
+		document.getElementById(Id).style.backgroundColor = "#2196F3";}
+	else{
+		ctrl=true;
+		document.getElementById(Id).style.backgroundColor = "transparent";}
+	}
+function dBoton(){
+	document.getElementByClassName("bAgregar").style.backgroundColor = "transparent";}
+function plSave(){
+	alert("Guardado");
+}
+
+function plAdd(){
+	alert("Funcion en construccion");
 }
