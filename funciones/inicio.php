89510b31ba58367c2cc1bd9cd0c68455d26b05ac
zzz <Kazehaya@live.cl>
2016-09-16 18:57:17 -0300

    Listo Para Ordenar

---
+++ funciones/inicio.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+
+	//variables 
+	
+	
+	include('funciones.php');
+	
+	
+	// efectos de la interaccion que tenga el usurio
+	
+	
+
+	fun_get_consultas();
+	
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<?php
+// inicia la session de usuario
+session_start();
+header('Content-type: text/html; charset=utf-8');
+
+include_once "conexion.php";
+
+$pag_base_directorio = dirname(__FILE__);
+
+include('configuraciones.php');
+include('funciones/inicio.php');
+
+?>
