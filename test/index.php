3c74b7ef1ea4bf1b3b6cac8490ec84960ed4ee72
Ignacio <ilincolaovenegas@gmail.com>
2016-10-19 12:34:40 -0400

    Actualización del login

--- test/index.php
+++ test/index.php
@@ -7,7 +7,7 @@ function verificar_login($user,$password,&$result){
     include("./php/conex.inc");
     $query = pg_query($dbconn, "SELECT * FROM \"tUsuarios\" WHERE \"Usuario\" = '$user' and \"Password\" = '$password'");
     $count = 0;
-    if(!empty($row = pg_fetch_object($query))) 
+    if(!empty($row = pg_fetch_object($query))) #Aqui habia un while. Esto es kinda redunte. El nombre del usuario deberia ser unico, por lo tanto, Siempre va a haber 1 asi que lo arregle, para que solo haga la consulta. Sin necesidad de usar un ciclo while.
     {
         $result = $row;
         return 1;
@@ -38,9 +38,9 @@ else
 {
     //Requiere que cargue el archivo conexion, con la informacion de la DB, para que el resto de la pagina carge.
     // Carga las variables y las funciones
-    require './php/Variables.php';
-    require './php/funciones.php';
+    require '../php/Variables.php';
+    require '../php/funciones.php';
     // Arma el html de la pagina.
-    require './php/Inicio_Carga.php';
+    require '../php/Inicio_Carga.php';
 }
 ?>
