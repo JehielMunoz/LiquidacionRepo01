8a0cda6072f7d9e3e6d553261caf95f07e57f70d
vonbinner <vonbinner@gmail.com>
2016-11-23 21:41:21 -0400

    Avance log 2
    Ahora el log registra al hacer cambios en la tabla de impuesto a la renta y cuando se modifican o eliminan licencias.

--- php/Desactivar_licencias.php
+++ php/Desactivar_licencias.php
@@ -1,4 +1,5 @@
 <?php 
+include("funciones.php");
 $url = explode("/",$_SERVER['HTTP_REFERER']);
 if (end($url)=="Licencias.php")
 {
@@ -10,6 +11,11 @@ if (end($url)=="Licencias.php")
             $query = pg_query($dbconn, "UPDATE \"tLicencias\" SET \"Dias\"=".$_POST['dias']." where \"id_Licencia\" =".$_POST['id_modificar']);
             header('location: ../html/Licencias.php');
             echo "1";
+        
+            $query = pg_query($dbconn, "SELECT * FROM \"tLicencias\" where \"id_Licencia\" =".$_POST['id_modificar']);
+            $fila = pg_fetch_assoc($query);
+            Escribir_Reporte("Se han modificado los dias de licencia del empleado con rut: ".$fila['Rut']." a ".$_POST['dias']." dias.");
+        
             exit();
     }
 
@@ -20,6 +26,11 @@ if (end($url)=="Licencias.php")
         $query = pg_query($dbconn, "UPDATE \"tLicencias\" SET \"Activo\"='FALSE' where \"id_Licencia\" =".$_GET['id_licencia']);
         header('location: ../html/Licencias.php');
         echo "2";
+        
+        $query2 = pg_query($dbconn, "SELECT * FROM \"tLicencias\" where \"id_Licencia\" =".$_GET['id_licencia']);
+        $fila = pg_fetch_assoc($query2);
+        Escribir_Reporte("Se ha eliminado una licencia del empleado con rut: ".$fila['Rut'].".");
+        
         exit();
     }
     else
